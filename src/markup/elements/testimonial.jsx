2122eb12817b2e872d453c463506a4922c11c9fa
ibraphem <ibraphem@gmail.com>
2021-11-23 22:10:46 +0100

    fix homepage

--- src/markup/elements/testimonial.jsx
+++ src/markup/elements/testimonial.jsx
@@ -5,6 +5,12 @@ import Slider from "react-slick";
 // Import Images
 import testShape from "../../images/testimonials/shape.png"
 import testPic1 from "../../images/testimonials/pic1.jpg"
+import dev1 from "../../images/testimonials/dev1.png"
+import dev2 from "../../images/testimonials/dev2.png"
+import dev3 from "../../images/testimonials/dev3.png"
+import dev4 from "../../images/testimonials/dev4.png"
+import dev5 from "../../images/testimonials/dev5.png"
+import dev6 from "../../images/testimonials/dev6.png"
 import testPic2 from "../../images/testimonials/pic2.jpg"
 import testPic3 from "../../images/testimonials/pic3.jpg"
 import testPic4 from "../../images/testimonials/pic4.jpg"
@@ -42,12 +48,12 @@ class testimonialSection extends Component{
 								<div className="thumb-wraper">
 									<img className="bg-img" src={testShape} alt=""/>
 									<ul>
-										<li data-member="1"><Link to="#"><img src={testPic1} alt=""/></Link></li>
-										<li data-member="2"><Link to="#"><img src={testPic2} alt=""/></Link></li>
-										<li data-member="3"><Link to="#"><img src={testPic3} alt=""/></Link></li>
-										<li data-member="4"><Link to="#"><img src={testPic4} alt=""/></Link></li>
-										<li data-member="5"><Link to="#"><img src={testPic5} alt=""/></Link></li>
-										<li data-member="6"><Link to="#"><img src={testPic6} alt=""/></Link></li>
+										<li data-member="1"><Link to="#"><img src={dev1} alt=""/></Link></li>
+										<li data-member="2"><Link to="#"><img src={dev2} alt=""/></Link></li>
+										<li data-member="3"><Link to="#"><img src={dev3} alt=""/></Link></li>
+										<li data-member="4"><Link to="#"><img src={dev4} alt=""/></Link></li>
+										<li data-member="5"><Link to="#"><img src={dev5} alt=""/></Link></li>
+										<li data-member="6"><Link to="#"><img src={dev6} alt=""/></Link></li>
 									</ul>
 								</div>
 							</div>
@@ -55,74 +61,18 @@ class testimonialSection extends Component{
 								<Slider {...settings} className="testimonial-slide">
 									<div className="slider-item">
 										<div className="testimonial-bx">
+										<div className="client-info">
+												<h5 className="name">Leverage On Our API</h5>
+											</div>
 											<div className="testimonial-content">
-												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena ssuspendisse ultrices gravida.</p>
+												<p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+												Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena ssuspendisse ultrices gravida.</b></p>
 											</div>
-											<div className="client-info">
-												<h5 className="name">John Deo</h5>
-												<p>patient</p>
-											</div>
-											<div className="quote-icon">
-												<i className="fas fa-quote-left"></i>
-											</div>
+											
+										
 										</div>
 									</div>
-									<div className="slider-item">
-										<div className="testimonial-bx">
-											<div className="testimonial-content">
-												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena ssuspendisse ultrices gravida.</p>
-											</div>
-											<div className="client-info">
-												<h5 className="name">John Deo</h5>
-												<p>patient</p>
-											</div>
-											<div className="quote-icon">
-												<i className="fas fa-quote-left"></i>
-											</div>
-										</div>
-									</div>
-									<div className="slider-item">
-										<div className="testimonial-bx">
-											<div className="testimonial-content">
-												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena ssuspendisse ultrices gravida.</p>
-											</div>
-											<div className="client-info">
-												<h5 className="name">John Deo</h5>
-												<p>patient</p>
-											</div>
-											<div className="quote-icon">
-												<i className="fas fa-quote-left"></i>
-											</div>
-										</div>
-									</div>
-									<div className="slider-item">
-										<div className="testimonial-bx">
-											<div className="testimonial-content">
-												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena ssuspendisse ultrices gravida.</p>
-											</div>
-											<div className="client-info">
-												<h5 className="name">John Deo</h5>
-												<p>patient</p>
-											</div>
-											<div className="quote-icon">
-												<i className="fas fa-quote-left"></i>
-											</div>
-										</div>
-									</div>
-									<div className="slider-item">
-										<div className="testimonial-bx">
-											<div className="testimonial-content">
-												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecena ssuspendisse ultrices gravida.</p>
-											</div>
-											<div className="client-info">
-												<h5 className="name">John Deo</h5>
-												<p>patient</p>
-											</div>
-											<div className="quote-icon">
-												<i className="fas fa-quote-left"></i>
-											</div>
-										</div>
-									</div>
+									
 								</Slider>
 							</div>	 
 						</div>	 
